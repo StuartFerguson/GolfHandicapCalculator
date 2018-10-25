e7d207a5a4dfa6f18053f13f5bd73c06b555365b
StuartFerguson <stuart_ferguson1@btinternet.com>
2018-10-25 12:50:24 +0100

    Initial changes for club management

---
+++ API/ManagementAPI/src/ManagementAPI.Service/Controllers/ClubConfigurationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using ManagementAPI.Service.DataTransferObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementAPI.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubConfigurationController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostClubConfiguration(CreateClubConfigurationRequest request, CancellationToken cancellationToken)
+        {
+            return this.StatusCode((Int32) HttpStatusCode.NotImplemented);
+        }
+    }
+}
