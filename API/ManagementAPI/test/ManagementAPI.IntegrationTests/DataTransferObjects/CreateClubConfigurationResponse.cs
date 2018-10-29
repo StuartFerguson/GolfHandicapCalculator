6447acc761189c5eb5a9b9dd6c6dc502b4dab3c8
StuartFerguson <stuart_ferguson1@btinternet.com>
2018-10-29 15:14:49 +0000

    Addition of integration tests

---
+++ API/ManagementAPI/test/ManagementAPI.IntegrationTests/DataTransferObjects/CreateClubConfigurationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManagementAPI.IntegrationTests.DataTransferObjects
+{
+    public class CreateClubConfigurationResponse
+    {
+        /// <summary>
+        /// Gets or sets the club configuration identifier.
+        /// </summary>
+        /// <value>
+        /// The club configuration identifier.
+        /// </value>
+        public Guid ClubConfigurationId { get; set; }
+    }
+}
